3e4e58488712f862308e2ef6a22a8727740722a3
Andrew R Chen <hello@ndrewr.com>
2020-01-23 19:06:00 -0800

    Implement local action-one on node runner.

---
+++ .github/actions/local-action-one/index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+// const github = require('@actions/github');
+const exec = require('child_process').exec;
+// const fs = require('fs');
+
+try {
+    installGoods(() => {
+        sayDone();
+    });
+} catch (e) {
+    core.setFailed(error.message);
+}
+
+function sayDone() {
+    console.log('I think we are done!');
+}
+
+function installGoods(next) {
+    const cmd1 = `
+        curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+        echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    `;
+    const cmd2 = `sudo apt update && sudo apt install yarn`;
+    const cmd3 = `echo 'Check Yarn install....' && yarn --version`;
+    exec(`${cmd1} && ${cmd2} && ${cmd3}`, (error, stdout, stderr) => {
+        if (error) throw stderr;
+        next();
+    });
+}
