728629e68ef2635f389c054828886e396ea4c0e5
Andrew R Chen <hello@ndrewr.com>
2020-01-28 18:52:27 -0800

    Add commands to run check deploy to action code.

--- .github/actions/ci-validate-prod/entrypoint.sh
+++ .github/actions/ci-validate-prod/entrypoint.sh
@@ -13,9 +13,9 @@ sfdx force:auth:sfdxurl:store -f prod_auth_url.txt -a Prod && rm prod_auth_url.t
 
 #Convert to MDAPI format for validation against prod
 echo "Converting to MDAPI format..."
-# sfdx force:source:convert -d validate_prod -r force-app
+sfdx force:source:convert -d validate_prod -r force-app
 #Simulate deployment to prod & run all tests
 echo "Validating against production by simulating a deployment & running all tests..."
-# sfdx force:mdapi:deploy -c -d validate_prod -u Prod -l RunLocalTests -w -1
+sfdx force:mdapi:deploy -c -d validate_prod -u Prod -l RunLocalTests -w -1
 
 echo "CI Validate Prod action completed."
