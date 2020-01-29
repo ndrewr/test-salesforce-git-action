2db32672509c4a1998e6fc4a70ba0effc48f5555
Andrew R Chen <hello@ndrewr.com>
2020-01-28 18:20:39 -0800

    Fix auth key env var ref btwn workflow, action files.

--- .github/actions/ci-validate-prod/entrypoint.sh
+++ .github/actions/ci-validate-prod/entrypoint.sh
@@ -5,7 +5,7 @@
 
 # Get the private url from environment variable, create required file for cmd
 echo "Setting up Prod Connection..."
-openssl enc -d -aes-256-cbc -md md5 -in prod_auth_url.txt.enc -out prod_auth_url.txt -k $AUTH_FILE_KEY
+openssl enc -d -aes-256-cbc -md md5 -in prod_auth_url.txt.enc -out prod_auth_url.txt -k ${{ inputs.auth-file-key }}
 
 # Authenticate to salesforce Prod org
 echo "Authenticating..."
