bddcb1a18ef70b53f1df5b9066e40266f16e3593
Andrew R Chen <hello@ndrewr.com>
2020-02-24 17:46:16 -0800

    Modify local action to set dynamic auth file ref.

--- .github/actions/ci-validate-prod/entrypoint.sh
+++ .github/actions/ci-validate-prod/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -l
 
+# export TEMPY="${TEMPY^^}plus"
+AUTH_FILE_KEY=$1
+TARGET_ALIAS=$2
+AUTH_URL="${TARGET_ALIAS,,}_auth_url.txt.enc"
+
 # Get the private url from environment variable, create required file for cmd
 echo "Setting up Prod Connection..."
 
@@ -10,7 +15,9 @@ echo "Target org alias..." $2
 # printf "%s" "$AUTH_URL_ENC" > prod_auth_url.txt.enc
 
 if test -f prod_auth_url.txt.enc ; then
-    openssl enc -d -aes-256-cbc -md md5 -in prod_auth_url.txt.enc -out prod_auth_url.txt -k $1
+    # openssl enc -d -aes-256-cbc -md md5 -in prod_auth_url.txt.enc -out prod_auth_url.txt -k $1
+    openssl enc -d -aes-256-cbc -md md5 -in $AUTH_URL -out prod_auth_url.txt -k $AUTH_FILE_KEY
+
 else
     echo "Required file missing: prod_auth_url.txt.enc! Exiting!"
     exit 1
