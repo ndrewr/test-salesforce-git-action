1f4ce618b8897998e66630c71deb80d01a96011b
Andrew R Chen <hello@ndrewr.com>
2020-02-24 18:46:28 -0800

    Use var ref for auth_url file in local action.

--- .github/actions/ci-validate-prod/entrypoint.sh
+++ .github/actions/ci-validate-prod/entrypoint.sh
@@ -6,17 +6,18 @@ echo "Setting up Prod Connection..."
 # export TEMPY="${TEMPY^^}plus"
 AUTH_FILE_KEY=$1
 TARGET_ALIAS=$2
-AUTH_URL="${TARGET_ALIAS,,}_auth_url.txt.enc"
+AUTH_URL="${TARGET_ALIAS,,}_auth_url.txt"
+ENC_AUTH_URL="${AUTH_URL}.enc"
 
-echo "Target org alias... ${2}"
+echo "Target org alias... ${TARGET_ALIAS}"
 
 # For a remote action that tries to get value from passed env
 # echo -n "$AUTH_URL_ENC" > prod_auth_url.txt.enc
 # printf "%s" "$AUTH_URL_ENC" > prod_auth_url.txt.enc
 
-if test -f prod_auth_url.txt.enc ; then
+if test -f $ENC_AUTH_URL ; then
     # openssl enc -d -aes-256-cbc -md md5 -in prod_auth_url.txt.enc -out prod_auth_url.txt -k $1
-    openssl enc -d -aes-256-cbc -md md5 -in "$AUTH_URL" -out prod_auth_url.txt -k "$AUTH_FILE_KEY"
+    openssl enc -d -aes-256-cbc -md md5 -in "$ENC_AUTH_URL" -out "$AUTH_URL" -k "$AUTH_FILE_KEY"
 
 else
     echo "Required file missing: prod_auth_url.txt.enc! Exiting!"
