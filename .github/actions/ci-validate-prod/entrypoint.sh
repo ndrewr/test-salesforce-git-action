22d9e735890d594e57c39afdb5cf1a8c68f74808
Andrew R Chen <hello@ndrewr.com>
2020-02-24 18:29:42 -0800

    Try to address "bad substitution error" in local action.

--- .github/actions/ci-validate-prod/entrypoint.sh
+++ .github/actions/ci-validate-prod/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -l
 
+# Get the private url from environment variable, create required file for cmd
+echo "Setting up Prod Connection..."
+
 # export TEMPY="${TEMPY^^}plus"
 AUTH_FILE_KEY=$1
 TARGET_ALIAS=$2
 AUTH_URL="${TARGET_ALIAS,,}_auth_url.txt.enc"
 
-# Get the private url from environment variable, create required file for cmd
-echo "Setting up Prod Connection..."
-
-echo "Target org alias..." $2
+echo "Target org alias... ${2}"
 
 # For a remote action that tries to get value from passed env
 # echo -n "$AUTH_URL_ENC" > prod_auth_url.txt.enc
@@ -16,7 +16,7 @@ echo "Target org alias..." $2
 
 if test -f prod_auth_url.txt.enc ; then
     # openssl enc -d -aes-256-cbc -md md5 -in prod_auth_url.txt.enc -out prod_auth_url.txt -k $1
-    openssl enc -d -aes-256-cbc -md md5 -in $AUTH_URL -out prod_auth_url.txt -k $AUTH_FILE_KEY
+    openssl enc -d -aes-256-cbc -md md5 -in "$AUTH_URL" -out prod_auth_url.txt -k "$AUTH_FILE_KEY"
 
 else
     echo "Required file missing: prod_auth_url.txt.enc! Exiting!"
